2eacd9470b98401e8f009d531c78beea68910750
Jiaqi Li <ljiaqi@microsoft.com>
2025-09-29 14:07:48 -0700

    comment all things after docker

--- distros/ubuntu22.04/install.sh
+++ distros/ubuntu22.04/install.sh
@@ -46,90 +46,90 @@ if [ "$GPU" = "NVIDIA" ]; then
     # Install NCCL
     $COMPONENT_DIR/install_nccl.sh
     
-    # Install NVIDIA docker container
-    $COMPONENT_DIR/install_docker.sh
-systemsctl status docker
+#     # Install NVIDIA docker container
+#     $COMPONENT_DIR/install_docker.sh
+# systemsctl status docker
 
-    # Install DCGM
-    $COMPONENT_DIR/install_dcgm.sh
-systemsctl status docker
+#     # Install DCGM
+#     $COMPONENT_DIR/install_dcgm.sh
+# systemsctl status docker
 
 fi
 
-if [ "$GPU" = "AMD" ]; then
-    # Set up docker
-    apt-get install -y moby-engine
-    systemctl enable docker
-    systemctl restart docker
+# if [ "$GPU" = "AMD" ]; then
+#     # Set up docker
+#     apt-get install -y moby-engine
+#     systemctl enable docker
+#     systemctl restart docker
 
-    #install rocm software stack
-    $COMPONENT_DIR/install_rocm.sh    
-    #install rccl and rccl-tests
-    $COMPONENT_DIR/install_rccl.sh
-fi
+#     #install rocm software stack
+#     $COMPONENT_DIR/install_rocm.sh    
+#     #install rccl and rccl-tests
+#     $COMPONENT_DIR/install_rccl.sh
+# fi
 
-# install AMD libs
-$COMPONENT_DIR/install_amd_libs.sh
-systemsctl status docker
+# # install AMD libs
+# $COMPONENT_DIR/install_amd_libs.sh
+# systemsctl status docker
 
-# install Intel libraries
-$COMPONENT_DIR/install_intel_libs.sh
-systemsctl status docker
+# # install Intel libraries
+# $COMPONENT_DIR/install_intel_libs.sh
+# systemsctl status docker
 
-# cleanup downloaded tarballs - clear some space
-rm -rf *.tgz *.bz2 *.tbz *.tar.gz *.run *.deb *_offline.sh
-rm -rf /tmp/MLNX_OFED_LINUX* /tmp/*conf*
-rm -rf /var/intel/ /var/cache/*
-rm -Rf -- */
+# # cleanup downloaded tarballs - clear some space
+# rm -rf *.tgz *.bz2 *.tbz *.tar.gz *.run *.deb *_offline.sh
+# rm -rf /tmp/MLNX_OFED_LINUX* /tmp/*conf*
+# rm -rf /var/intel/ /var/cache/*
+# rm -Rf -- */
 
-# optimizations
-$COMPONENT_DIR/hpc-tuning.sh
-systemsctl status docker
+# # optimizations
+# $COMPONENT_DIR/hpc-tuning.sh
+# systemsctl status docker
 
-# Install AZNFS Mount Helper
-$COMPONENT_DIR/install_aznfs.sh
-systemsctl status docker
+# # Install AZNFS Mount Helper
+# $COMPONENT_DIR/install_aznfs.sh
+# systemsctl status docker
 
-# install diagnostic script
-$COMPONENT_DIR/install_hpcdiag.sh
-systemsctl status docker
+# # install diagnostic script
+# $COMPONENT_DIR/install_hpcdiag.sh
+# systemsctl status docker
 
-# install monitor tools
-$COMPONENT_DIR/install_monitoring_tools.sh
-systemsctl status docker
+# # install monitor tools
+# $COMPONENT_DIR/install_monitoring_tools.sh
+# systemsctl status docker
 
-# install persistent rdma naming
-$COMPONENT_DIR/install_azure_persistent_rdma_naming.sh
-systemsctl status docker
+# # install persistent rdma naming
+# $COMPONENT_DIR/install_azure_persistent_rdma_naming.sh
+# systemsctl status docker
 
-# copy test file
-$COMPONENT_DIR/copy_test_file.sh
-systemsctl status docker
+# # copy test file
+# $COMPONENT_DIR/copy_test_file.sh
+# systemsctl status docker
 
-# install Azure/NHC Health Checks
-$COMPONENT_DIR/install_health_checks.sh "$GPU"
-systemsctl status docker
+# # install Azure/NHC Health Checks
+# $COMPONENT_DIR/install_health_checks.sh "$GPU"
+# systemsctl status docker
 
-# disable cloud-init
-$COMPONENT_DIR/disable_cloudinit.sh
-systemsctl status docker
+# # disable cloud-init
+# $COMPONENT_DIR/disable_cloudinit.sh
+# systemsctl status docker
 
-# SKU Customization
-$COMPONENT_DIR/setup_sku_customizations.sh
-systemsctl status docker
+# # SKU Customization
+# $COMPONENT_DIR/setup_sku_customizations.sh
+# systemsctl status docker
 
-# scan vulnerabilities using Trivy
-$COMPONENT_DIR/trivy_scan.sh
-systemsctl status docker
+# # scan vulnerabilities using Trivy
+# $COMPONENT_DIR/trivy_scan.sh
+# systemsctl status docker
 
-# diable auto kernel updates
-./disable_auto_upgrade.sh
-systemsctl status docker
+# # diable auto kernel updates
+# ./disable_auto_upgrade.sh
+# systemsctl status docker
 
-# Disable Predictive Network interface renaming
-./disable_predictive_interface_renaming.sh
-systemsctl status docker
+# # Disable Predictive Network interface renaming
+# ./disable_predictive_interface_renaming.sh
+# systemsctl status docker
 
-# clear history
-# Uncomment the line below if you are running this on a VM
-# $UTILS_DIR/clear_history.sh
+# # clear history
+# # Uncomment the line below if you are running this on a VM
+# # $UTILS_DIR/clear_history.sh
